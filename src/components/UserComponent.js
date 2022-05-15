4f2b41f59173903db8f0f1bc01c877fe7db9cff5
altisseront <altisseront@gmail.com>
2022-05-15 05:59:32 -0400

    putting it together

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class UserComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age:this.props.age
+        }
+    }
     render(){
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.birthday}>Birthday!</button>
             </div>
         )
+    }
+    birthday = (e) => {
+        this.setState({age: parseInt(this.state.age) +1})
     }
 }
 
